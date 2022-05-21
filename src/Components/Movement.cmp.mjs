92ca9fc608f42c74088fe546de52a6c2405c269e
jiamingwangnet <jiamingwang.net@gmail.com>
2022-05-21 12:53:14 +1000

    added custom keybind for movement component

--- src/Components/Movement.cmp.mjs
+++ src/Components/Movement.cmp.mjs
@@ -4,11 +4,12 @@ import { BoxCollider } from "./BoxCollider.cmp.mjs";
 import { Physics } from "./Physics.cmp.mjs";
 
 export class Movement extends Component {
-    constructor(holder, speed, jump) {
+    constructor(holder, speed, jump, keybinds = {JUMP: "w" , LEFT: "a", RIGHT: "d"}) {
         super(holder);
         this._speed = speed;
         this._jump = jump;
         this._moveVector = new Vector(0,0);
+        this._keybinds = keybinds;
     }
 
     __Start__() {
@@ -26,29 +27,22 @@ export class Movement extends Component {
     __Update__() {
         const groundCheck = this._holder.GetComponent(BoxCollider).CollideAll();
 
-        if (this._holder._game.keystrokes["w"] && !(Math.abs(this.holder.GetComponent(Physics).baseVelocity.y) > 0) && groundCheck.bottom) {           
+        if (this._holder._game.keystrokes[this._keybinds.JUMP] && !(Math.abs(this.holder.GetComponent(Physics).baseVelocity.y) > 0) && groundCheck.bottom) {           
             this._moveVector.y = -1;
         }
         else {
             this._moveVector.y = 0;
         }
 
-        if (this._holder._game.keystrokes["d"]) {
+        if (this._holder._game.keystrokes[this._keybinds.RIGHT]) {
             this._moveVector.x = 1;
         }
-        else if (this._holder._game.keystrokes["a"]) {
+        else if (this._holder._game.keystrokes[this._keybinds.LEFT]) {
             this._moveVector.x = -1;
         }
         else {
             this._moveVector.x = 0;
         }
-
-        // if (this._holder._game.keystrokes["s"]) {
-        //     this._StartMoveX(5);
-        // }
-        // else {
-        //     this._StopMoveX();
-        // }
 
         this._Move(this._moveVector);
     }
