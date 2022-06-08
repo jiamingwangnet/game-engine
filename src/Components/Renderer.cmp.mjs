6fe67689ec9efa5d752727a162c35d2ebdebb658
jiamingwangnet <jiamingwang.net@gmail.com>
2022-06-08 21:39:51 +1000

    added width height getters and setters

--- src/Components/Renderer.cmp.mjs
+++ src/Components/Renderer.cmp.mjs
@@ -73,6 +73,12 @@ export class Renderer extends Component
         this._UpdateImage();
     }
 
+    get width(){return this._width;}
+    set width(width){this._width = width;}
+
+    get height(){return this._height;}
+    set height(height){this._height = height;}
+
     /**
      * Updates and reloads the image
      * @memberof Renderer
