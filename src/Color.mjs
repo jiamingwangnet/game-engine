a23af240440142a280a491312ae917f223118bd8
Jiaming Wang <jiamingwang.net@gmail.com>
2022-06-09 09:45:26 +1000

    fixed color errors

--- src/Color.mjs
+++ src/Color.mjs
@@ -3,15 +3,15 @@ export class Color {
         if ((r > 0xff || g > 0xff || b > 0xff || a > 0xff) || (r < 0 || g < 0 || b < 0 || a < 0)) {
             throw new Error("Colors must be < 256 and >= 0");
         }
-        this.color = r && g && b && a ? r * 0x1000000 + g * 0x10000 + b * 100 + a : raw;
+        this.color = r && g && b && a ? r * 0x1000000 + g * 0x10000 + b * 0x100 + a : raw;
     }
 
     get rawColor() { return this.color }
     get colorRGB() {return `rgb(${this.r}, ${this.g}, ${this.b}, ${this.a / 255})`;}
     get colorHEX() {return `#${(this.r < 0x10 ? "0" : "") + this.r.toString(16)}${(this.g < 0x10 ? "0" : "") + this.g.toString(16)}${(this.b < 0x10 ? "0" : "") + this.b.toString(16)}${(this.a < 0x10 ? "0" : "") + this.b.toString(16)}`;}
-    get r() {return (this.color & 0xff000000) >> 32;}
-    get g() {return (this.color & 0x00ff0000) >> 16;}
-    get b() {return (this.color & 0x0000ff00) >> 8;}
+    get r() {return (this.color & 0xff000000) >>> 24;}
+    get g() {return (this.color & 0x00ff0000) >>> 16;}
+    get b() {return (this.color & 0x0000ff00) >>> 8;}
     get a() {return (this.color & 0x000000ff);}
     set r(r) {this.color = this.a + this.b * 0x100 + this.g * 0x10000 + r * 0x1000000;}
     set g(g) {this.color = this.a + this.b * 0x100 + g * 0x10000 + this.r * 0x1000000;}
