ce15896b8c0244884b62f765a7f3f549fa59ba55
jiamingwangnet <jiamingwang.net@gmail.com>
2022-06-10 21:19:26 +1000

    color test

--- example/js/Player.gobj.mjs
+++ example/js/Player.gobj.mjs
@@ -2,6 +2,7 @@ import { GameObject } from "../../src/GameObject.mjs";
 import { Physics } from "../../src/Components/Physics.cmp.mjs";
 import { BoxCollider } from "../../src/Components/BoxCollider.cmp.mjs";
 import { Movement } from "../../src/Components/Movement.cmp.mjs";
+import { Color } from "../../src/Color.mjs";
 
 export class Player extends GameObject
 {
@@ -12,10 +13,10 @@ export class Player extends GameObject
 
     __Start__()
     {  
-        this._renderer.SetPixels("#FFFFFF", 0, 0, this._width, this._height);
-        this._renderer.SetPixels("#000000", 10, 10, 10, 10);
-        this._renderer.SetPixels("#000000", 30, 10, 10, 10);
-        this._renderer.SetPixels("#000000", 5, 30, 40, 5);
+        this._renderer.SetPixels(new Color(255, 255, 255, 1), 0, 0, this._width, this._height);
+        this._renderer.SetPixels(new Color(0,0,0,1), 10, 10, 10, 10);
+        this._renderer.SetPixels(new Color(0,0,0,1), 30, 10, 10, 10);
+        this._renderer.SetPixels(new Color(0,0,0,1), 5, 30, 40, 5);
         
         this.AddComponent(new Physics(this, this._game));
         this.AddComponent(new BoxCollider(this.width, this.height, this, this._game, this.GetComponent(Physics)));
--- example/js/main.js
+++ example/js/main.js
@@ -15,40 +15,40 @@ game.__Load__ = () => {
     const player = new Player(100, 500, game, "player");
     game.AddGameObject(player);
 
-    const floor = new Block(-10000, window.innerHeight - 40, 20000, 50, "#096e00", game, "floor");
+    const floor = new Block(-10000, window.innerHeight - 40, 20000, 50, new Color(0x09, 0x6e, 0x00, 1), game, "floor");
     game.AddGameObject(floor);
 
-    const wall = new Block(350, window.innerHeight - 220, 20, 200, "gray", game, "wall");
+    const wall = new Block(350, window.innerHeight - 220, 20, 200, new Color(0x80, 0x80, 0x80, 1), game, "wall");
     game.AddGameObject(wall);
 
-    const wall2 = new Block(330, window.innerHeight - 170, 20, 150, "gray", game, "wall2");
+    const wall2 = new Block(330, window.innerHeight - 170, 20, 150, new Color(0x80, 0x80, 0x80, 1), game, "wall2");
     game.AddGameObject(wall2);
 
-    const wall3 = new Block(310, window.innerHeight - 120, 20, 100, "gray", game, "wall3");
+    const wall3 = new Block(310, window.innerHeight - 120, 20, 100, new Color(0x80, 0x80, 0x80, 1), game, "wall3");
     game.AddGameObject(wall3);
 
-    const wall4 = new Block(370, window.innerHeight - 170, 20, 150, "gray", game, "wall4");
+    const wall4 = new Block(370, window.innerHeight - 170, 20, 150, new Color(0x80, 0x80, 0x80, 1), game, "wall4");
     game.AddGameObject(wall4);
 
-    const wall5 = new Block(390, window.innerHeight - 120, 20, 100, "gray", game, "wall5");
+    const wall5 = new Block(390, window.innerHeight - 120, 20, 100, new Color(0x80, 0x80, 0x80, 1), game, "wall5");
     game.AddGameObject(wall5);
 
-    const higherFloor = new Block(1100, window.innerHeight - 220, 500, 200, "blue", game, "higherFloor")
+    const higherFloor = new Block(1100, window.innerHeight - 220, 500, 200, new Color(0,0,0xff, 1), game, "higherFloor")
     game.AddGameObject(higherFloor);
 
-    const higherFloor2 = new Block(1000, window.innerHeight - 220, 50, 50, "cyan", game, "higherFloor2")
+    const higherFloor2 = new Block(1000, window.innerHeight - 220, 50, 50, new Color(0,0xff,0xff, 1), game, "higherFloor2")
     game.AddGameObject(higherFloor2);
-    const higherFloor3 = new Block(940, window.innerHeight - 220, 50, 50, "dodgerblue", game, "higherFloor3")
+    const higherFloor3 = new Block(940, window.innerHeight - 220, 50, 50, new Color(0x1e, 0x90, 0xff, 1), game, "higherFloor3")
     game.AddGameObject(higherFloor3);
-    const higherFloor4 = new Block(880, window.innerHeight - 220, 50, 50, "navy", game, "higherFloor4")
+    const higherFloor4 = new Block(880, window.innerHeight - 220, 50, 50, new Color(0, 0, 0x80, 1), game, "higherFloor4")
     game.AddGameObject(higherFloor4);
-    const higherFloor5 = new Block(820, window.innerHeight - 220, 50, 50, "teal", game, "higherFloor5")
+    const higherFloor5 = new Block(820, window.innerHeight - 220, 50, 50, new Color(0, 0x80, 0x80, 1), game, "higherFloor5")
     game.AddGameObject(higherFloor5);
 
-    const image = new Block(700, window.innerHeight - 220, 50, 50, "", game, "image", "./assets/test.png");
+    const image = new Block(700, window.innerHeight - 220, 50, 50, new Color(0,0,0,0), game, "image", "./assets/test.png");
     game.AddGameObject(image);
 
-    const roof = new Block(450, window.innerHeight - 210, 1000, 10, "brown", game, "roof");
+    const roof = new Block(450, window.innerHeight - 210, 1000, 10, new Color(0xa5, 0x2a, 0x2a, 1), game, "roof");
     game.AddGameObject(roof);
 
     const audio = new AudioObj("./assets/test.wav", "audio", game);
@@ -74,7 +74,7 @@ game.__Update__ = () => {
 
     if(game.input.GetButtonPress("left"))
     {
-        const block = new Block(game.input.ScreenToWorldPosition(game.input.mousePosition).x, game.input.ScreenToWorldPosition(game.input.mousePosition).y, 10, 10, "red", game, "block");
+        const block = new Block(game.input.ScreenToWorldPosition(game.input.mousePosition).x, game.input.ScreenToWorldPosition(game.input.mousePosition).y, 10, 10, new Color(255, 0, 0, 1), game, "block");
         game.AddGameObject(block);
     }
 
@@ -90,13 +90,13 @@ game.__Update__ = () => {
         const g = Math.floor(Math.random() * 256);
         const b = Math.floor(Math.random() * 256);
 
-        const block = new Block(game.input.ScreenToWorldPosition(game.input.mousePosition).x + x, game.input.ScreenToWorldPosition(game.input.mousePosition).y + y, w, h, `rgb(${r},${g},${b})`, game, "peepee");
+        const block = new Block(game.input.ScreenToWorldPosition(game.input.mousePosition).x + x, game.input.ScreenToWorldPosition(game.input.mousePosition).y + y, w, h, new Color(r, g, b, 1), game, "peepee");
         game.AddGameObject(block);
     }
 
     if(game.input.GetButtonPress("middle"))
     {
-        const physicsBlock = new Block(game.input.ScreenToWorldPosition(game.input.mousePosition).x, game.input.ScreenToWorldPosition(game.input.mousePosition).y, 30, 30, "#00ff00", game, "block");
+        const physicsBlock = new Block(game.input.ScreenToWorldPosition(game.input.mousePosition).x, game.input.ScreenToWorldPosition(game.input.mousePosition).y, 30, 30, new Color(0, 255, 0, 1), game, "block");
         physicsBlock.AddComponent(new Physics(physicsBlock, game, 0.8));
         game.AddGameObject(physicsBlock);
     }
@@ -112,7 +112,7 @@ game.__Update__ = () => {
         const g = Math.floor(Math.random() * 256);
         const b = Math.floor(Math.random() * 256);
 
-        const block = new Block(game.input.ScreenToWorldPosition(game.input.mousePosition).x + x, game.input.ScreenToWorldPosition(game.input.mousePosition).y + y, w, h, `rgb(${r},${g},${b})`, game, "peepee");
+        const block = new Block(game.input.ScreenToWorldPosition(game.input.mousePosition).x + x, game.input.ScreenToWorldPosition(game.input.mousePosition).y + y, w, h, new Color(r, g, b, 1), game, "peepee");
         block.AddComponent(new Physics(block, game, 0.48));
         game.AddGameObject(block);
     }
@@ -133,3 +133,4 @@ window.Physics = Physics;
 window.BoxCollider = BoxCollider;
 window.Block = Block;
 window.Player = Player;
+window.Color = Color;
